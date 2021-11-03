40c5bbbf4c4a7edf63a48a69d068063818976a43
Iury Souza <iury.souza@klarna.com>
2021-11-03 20:19:38 -0300

    add basic components and single activity setup

--- app/src/main/java/com/iurysza/ui/MainActivity.kt
+++ app/src/main/java/com/iurysza/ui/MainActivity.kt
@@ -1,47 +1,33 @@
 package com.iurysza.ui
 
 import android.annotation.SuppressLint
-import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.webkit.WebView
 import android.widget.Button
-import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
 import com.iurysza.CustomWebViewClient
 import com.iurysza.R
-import com.iurysza.WebviewPackageName
 import com.iurysza.proxy.CustomProxyController
 
 class MainActivity : AppCompatActivity() {
-    private val rootView: LinearLayout by lazy { findViewById(R.id.activityRoot) }
     private val urlInputText by lazy { findViewById<TextInputEditText>(R.id.textUrl) }
     private val loadUrlBtn by lazy { findViewById<Button>(R.id.loadUrlButton) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        loadUrlBtn.setOnClickListener {
-            startActivity(Intent(this, SecondActivity::class.java))
-//            Log.e(TAG, urlInputText.text.toString())
-//            val url = urlInputText.text.toString()
-//            webView.loadUrl("""https://$url""")
-        }
+        val webView = findViewById<WebView>(R.id.webview).setupWebview()
 
-        if (!WebviewPackageName.isRandom) return
-
-        val webView = WebView(this).apply {
-            layoutParams = LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.MATCH_PARENT
-            )
-        }
-        rootView.addView(webView)
-        webView.setupWebview()
         webView.loadUrl("https://www.google.com")
-
+        loadUrlBtn.setOnClickListener {
+            Log.e(TAG, urlInputText.text.toString())
+            webView.loadUrl(urlInputText.text.toString())
+        }
     }
 
+    val TAG = "PROXY_TEST"
 
     @SuppressLint("SetJavaScriptEnabled")
     private fun WebView.setupWebview(): WebView = apply {
@@ -52,5 +38,3 @@ class MainActivity : AppCompatActivity() {
         settings.loadWithOverviewMode = true
     }
 }
-
-val TAG = "PROXY_TEST"
